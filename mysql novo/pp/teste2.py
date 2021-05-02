9ebf30cf3712fc24963f6b61966562ec0483fe1e
Pedroaaaa <82984609+Pedroaaaa@users.noreply.github.com>
2021-05-02 08:08:41 -0300

    Delete teste2.py

--- mysql novo/pp/teste2.py
+++
@@ -1,8 +0,0 @@
-from cs50 import SQL
-
-
-db = SQL("mysql://root:pmm@2406@localhost:3306/uenf")
-
-buildingID = 74
-image = "kkk"
-db.execute('UPDATE buildings SET image = "%s" WHERE id = (%s)'% (image, buildingID))
